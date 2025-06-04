a5dd99efb894c5b33d8c22311b8264fb8e28b723
rafay45 <rafayy579@gmail.com>
2025-06-04 23:29:36 +0500

    🎆🧣Practicing

--- script.js
+++ script.js
@@ -197,3 +197,63 @@
 //     console.log(res);
 // }
 // asyncFunction("sucess")
+
+// async function example() {
+//   console.log("1");
+//   await new Promise(resolve => setTimeout(resolve, 5000)); // 2 sec ka wait
+//   console.log("2");
+// }
+// example();
+
+// const promise = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         resolve("PROMISED 1 RESOLVED !!")
+//     }, 6000)
+// })
+
+// const promise1 = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         resolve("PROMISED 2 RESOLVED !!")
+//     }, 3000)
+// })
+
+
+// async function getData() {
+//     // promise.then((response)=> console.log(response))
+//     const p1 = await promise
+//     console.log(p1)
+
+//     const p2 = await promise1
+//     console.log(p2)
+
+//     console.log("Hello World !");
+// }
+
+
+//PROMISED 2 Resolved
+//PROMISED 1 Resolved
+//Hello World !
+
+
+
+//PROMISED 1 Resolved
+//PROMISED 2 Resolved
+//Hello World !
+
+// getData()
+
+
+// Question:01 AWAIT kabhi bhi wait nh krta?
+// Basically "await" waits for the promise to resolve
+//  before moving to the next line of code.
+
+// Question:02 Timer apas mei sum hokar kese chal rha hai?
+// Although the timers start at the same time the next line of code
+//  won't run until the first awaited promise is resolved or rejected.
+
+// Question:03 Agar 2nd promise pehle resolve hogaya to first wale
+//  promise se pehle run q nh hoa ?
+// Because the first promise waits to be resolved or rejected
+//  the second promise is executed afterward.
+
+
